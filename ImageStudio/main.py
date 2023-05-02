027dd4f00f78210898ed6d0f9d9e493a80655b74
Art <artsanchezgarcia@gmail.com>
2023-05-02 14:45:03 +0200

    02/05

--- ImageStudio/main.py
+++ ImageStudio/main.py
@@ -1,7 +1,19 @@
 from pathlib import Path
 
 
-def rename(directory_list_files):
+# create a folder in the same fashion ("001") and put there all the images
+def create_directory():
+    counter = 0
+    new_folder = Path(f"{counter:03d}")
+    try:
+        new_folder.mkdir(parents=True)
+    except:
+        counter += 1
+    return new_folder
+
+
+def rename_images(directory_list_files):
+    new_folder = create_directory()
     rename_list = []
     for item in directory_list_files:
         if item.is_file():
@@ -12,9 +24,10 @@ def rename(directory_list_files):
         extension = item.suffix
         new_name = str(f"{rename_counter:03d}") + extension
         try:
-            item.rename(Path(image_folder, new_name))
+            item.rename(Path(new_folder, new_name))
         except:
-            pass
+            rename_counter += 1
+            item.rename(Path(new_folder, new_name))
         rename_counter += 1
     return rename_list
 
@@ -22,23 +35,15 @@ def rename(directory_list_files):
 main_folder = Path.cwd()
 image_folder = Path('ImageFolder')
 # if ImageFolder exists -->
-if image_folder.exists():
-    file_list = image_folder.iterdir()
-    # if there are image inside it
-    # Rename them
-    image_list = rename(file_list)
+if not image_folder.exists():
+    image_folder.mkdir(parents=True)
+    print('ImageFolder directory created. Run the code again.')
+    quit()
 
-# create a folder in the same fashion ("001") and put there all the images
-counter = 0
-try:
-    new_folder = Path(f"{counter:03d}")
-    new_folder.mkdir(parents=True)
-except:
-    counter += 1
-    new_folder = Path(f"{counter:03d}")
-    new_folder.mkdir(parents=True)
+file_list = image_folder.iterdir()
+# if there are image inside it
+# Rename them and move them
+image_list = rename_images(file_list)
 
-for item in
-#   Treat the images making copies with the desired effect and/or comparison mode
-
-## Effects: BW, extraction of colors (paleta), RGB total values, extraction of color (solo el lineart)
+# Treat the images making copies with the desired effect and/or comparison mode
+# Effects: BW, extraction of colors (paleta), RGB total values, extraction of color (solo el lineart)
