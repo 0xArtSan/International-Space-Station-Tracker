5ca637033a7296d9c913a7c3a26486cf5a674334
Art <artsanchezgarcia@gmail.com>
2023-05-08 11:09:53 +0200

    08/05

--- ImageStudio/functions.py
+++ ImageStudio/functions.py
@@ -1,7 +1,17 @@
 import cv2 as cv
 from pathlib import Path
 import extcolors
-import PIL
+from PIL import Image
+
+
+def no_webp(directory_list_files, directory):
+    for item in directory_list_files:
+        if item.is_file():
+            if item.suffix == '.webp':
+                image = Image.open(item).convert('RGB')
+                new_path = f'ImageFolder/{item.stem}.png'
+                image.save(new_path, 'png')
+    return directory.iterdir()
 
 
 # create a folder in the same fashion ("001") and put there all the images
@@ -11,7 +21,6 @@ def create_directory():
         counter += 1
     new_folder = Path(f"{counter:03d}")
     new_folder.mkdir(parents=True)
-
     return new_folder
 
 
@@ -19,7 +28,7 @@ def rename_images(directory_list_files):
     new_folder = create_directory()
     rename_list = []
     for item in directory_list_files:
-        if item.is_file():
+        if item.is_file() and item.suffix != '.webp':
             rename_list.append(item)
 
     rename_counter = 1
@@ -37,12 +46,13 @@ def rename_images(directory_list_files):
 
 def black_white(original_image):
     image = cv.imread(original_image)
-    BW_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    return BW_image
+    bw_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    return bw_image
 
 
+# TODO: crear una imagen de 3x3 con los colores extraidos
 def palette(original_image):
-    image = PIL.Image.open(original_image)
+    image = Image.open(original_image)
     color, pixel_count = extcolors.extract_from_image(image)
     color_palette = []
     counter = 0
@@ -53,17 +63,25 @@ def palette(original_image):
         if counter > 8:
             return color_palette
 
-
-def color_extraction(original_image):
+#TODO: crear una imagen sobre fondo blanco con las mismas dimensiones que la imagen original y el contorno dibujado
+def contour(original_image):
     image = cv.imread(original_image)
     image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     edges = cv.Canny(image_gray, 100, 200)
     contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(image, contours, -1, (0, 0, 0), 1)
-    cv.imshow('contours', image)
-    cv.waitKey(0)
-    # cv.imwrite(f'contour - {original_image}', edges)
-    cv.destroyAllWindows()
+    cv.drawContours(image_gray, contours, -1, (0, 0, 0), 1)
+
+
+def compound(original_image, modified_image):
+    image_1 = cv.imread(original_image)
+    image_2 = cv.imread(modified_image)
+    compounded_image = cv.hconcat([image_1, image_2])
+    return compounded_image
+
 
 ## Hay que guardar el contorno en un lugar diferente
-color_extraction('000/008.webp')
+# test_path = '000/001.jpg'
+# no_webp('000/001.png')
+# cv.imshow('bandw', bw_image)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
--- ImageStudio/main.py
+++ ImageStudio/main.py
@@ -1,7 +1,6 @@
 from functions import *
 
 
-
 main_folder = Path.cwd()
 image_folder = Path('ImageFolder')
 # if ImageFolder doesnt exists create the folder -->
@@ -13,7 +12,8 @@ if not image_folder.exists():
 file_list = image_folder.iterdir()
 # if there are image inside it
 # Rename them and move them
-image_list = rename_images(file_list)
+new_file_list = no_webp(file_list, image_folder)
+image_list = rename_images(new_file_list)
 
 
 # Treat the images making copies with the desired effect and/or comparison mode
